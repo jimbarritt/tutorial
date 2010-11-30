22d9e3a645aad2c80450ccef8f683a3b337e64c1
Jim Barritt <jim@planet-ix.com>
2010-11-30 12:35:26 +0000

    Adding emma coverage

--- java/java-101/src/main/java/org/jimbarritt/tutorial/java101/Greeter.java
+++ java/java-101/src/main/java/org/jimbarritt/tutorial/java101/Greeter.java
@@ -4,7 +4,7 @@ import static java.lang.String.format;
 
 public class Greeter {
 
-	public String hello(String name) {
+	public String hello(String name) {		
 		return format("Hello '%s'", name);
 	}
 
