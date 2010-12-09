e50f1f70bd93249cf912dffaffd8d19aa206e223
jimbarritt <jim@planet-ix.com>
2010-12-09 14:12:36 +0000

    Added alternate implementation of looping.

--- java/java-101/src/main/java/org/jimbarritt/tutorial/java101/Greeter.java
+++ java/java-101/src/main/java/org/jimbarritt/tutorial/java101/Greeter.java
@@ -4,12 +4,34 @@ import static java.lang.String.format;
 
 public class Greeter {
 
+	private boolean useForEachLoop = false;
+	
 	public String sayHelloTo(String... names) {
+		if (useForEachLoop) {
+			return buildStringWIthForEachLoop(names);
+		}
+		return buildStringWithIndexedLoop(names);
+	}
+
+	private String buildStringWithIndexedLoop(String... names) {
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=0; i< names.length; i++) {
+			sb.append(names[i]);	
+			if (i<names.length-1) {
+				sb.append(" ");
+			}
+		}
+		
+		return format("Hello %s", sb.toString());
+	}
+
+	private String buildStringWIthForEachLoop(String... names) {
 		StringBuilder sb = new StringBuilder();
 
 		for (String name : names) {
 			sb.append(name).append(" ");
-		}
+		}				
 
 		return format("Hello %s", sb.toString().trim());
 	}
