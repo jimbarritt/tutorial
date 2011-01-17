d26ded3fa90006369781eca5a378bf2a1c21de36
jimbarritt <jim@planet-ix.com>
2011-01-17 23:00:48 +0530

    Quantity-02: Same Units (10cm == 10cm)

--- java/object-relationships/src/test/java/com/jimbarritt/tutorial/oo_relationships/quantity/QuantityTest.java
+++ java/object-relationships/src/test/java/com/jimbarritt/tutorial/oo_relationships/quantity/QuantityTest.java
@@ -3,18 +3,19 @@ package com.jimbarritt.tutorial.oo_relationships.quantity;
 import org.junit.*;
 
 import static com.jimbarritt.tutorial.oo_relationships.quantity.Unit.centimetres;
-import static com.jimbarritt.tutorial.oo_relationships.quantity.Unit.metres;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
 public class QuantityTest {
 
     @Test
-    public void ten_centimetres_equals_one_tenth_of_a_metre() {
+    public void ten_centimetres_equals_ten_centimetres() {
         Quantity tenCentimetres = new Quantity(10, centimetres);
-        Quantity oneTenthOfAMetre = new Quantity(0.1, metres);
+        Quantity oneTenthOfAMetre = new Quantity(10, centimetres);
 
         assertThat(tenCentimetres, is(oneTenthOfAMetre));
     }
+
+
 
 }
