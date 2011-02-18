6647d096f192138e108097ab3a57b9c73fe22eff
Jim Barritt <jim@planet-ix.com>
2011-02-18 13:21:56 +0530

    Adding global css and linking everything up properly

--- javascript/javascript-mvc/src/main/java/implicit_declaration/source_loader.js
+++
@@ -1,9 +0,0 @@
-var sourceloader = sourceloader || {};
-
-
-sourceloader.loadSourceInto = function (href, divId) {
-    var http = new XMLHttpRequest();
-    http.open("GET", href, false);
-    http.send();
-    document.getElementById(divId).innerHTML = http.responseText
-}
