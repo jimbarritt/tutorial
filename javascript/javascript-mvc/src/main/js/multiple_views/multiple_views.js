dde8675fa46cfb019342874df6adcfc7540f9621
Jim Barritt <jim@planet-ix.com>
2011-03-24 17:15:27 +0530

    [JimB] Got automatic boxes working

--- javascript/javascript-mvc/src/main/js/multiple_views/multiple_views.js
+++ javascript/javascript-mvc/src/main/js/multiple_views/multiple_views.js
@@ -7,19 +7,37 @@ multiview.controller = function(dom) {
             console.log("Controller.init");
             var model = multiview.model("I have been initialised by the javascript!");
             initReadOnlyDisplayViews(model, dom);
+            initManualTextBoxViews(model, dom);
+            initAutomaticTextBoxViews(model, dom);
         }
     }
 
     function initReadOnlyDisplayViews(model, dom) {
         var displayViewElements = dom.getElementsByClassName("display-text");
-        for (var i=0; i<displayViewElements.length; i++) {
+        for (var i = 0; i < displayViewElements.length; i++) {
             multiview.readOnlyDisplayView(model, dom, displayViewElements[i]);
-        };
+        }
+    }
+
+    function initManualTextBoxViews(model, dom) {
+        var manualTextBoxElements = dom.getElementsByClassName("manual-update-text");
+        for (var i = 0; i < manualTextBoxElements.length; i++) {
+            multiview.manualTextBoxView(model, dom, manualTextBoxElements[i]);
+        }
+    }
+
+    function initAutomaticTextBoxViews(model, dom) {
+        var automaticTextBoxElements = dom.getElementsByClassName("automatic-update-text");
+        for (var i = 0; i < automaticTextBoxElements.length; i++) {
+            multiview.automaticTextBoxView(model, dom, automaticTextBoxElements[i]);
+        }
     }
 
     return self;
 }
 
+var someFunction = function() {
+};
 multiview.model = function(defaultText) {
 
     var text = defaultText;
@@ -27,6 +45,7 @@ multiview.model = function(defaultText) {
 
     var self = {
         addListener : function(viewCallbackFunction) {
+            console.log("Registering listener : " + viewCallbackFunction.name);
             listeners.push(viewCallbackFunction);
         },
 
@@ -36,6 +55,7 @@ multiview.model = function(defaultText) {
 
         setText : function(newText) {
             if (text !== newText) {
+                text = newText;
                 fireOnChangeEvent();
             }
         }
@@ -51,37 +71,101 @@ multiview.model = function(defaultText) {
 }
 
 multiview.automaticTextBoxView = function(model, dom, rootElement) {
+    var textAreaElement = rootElement.getElementsByTagName('textarea')[0];
 
+    var init = function() {
+        console.log("Initialising AutomaticTextBox view");
+        updateUI();
+        model.addListener(updateUI);
+        bindToControls();
+    }
+
+    var updateUI = function() {
+        if (getText() !== model.getText()) {
+            setText(model.getText());
+        }
+    }
+
+    var updateModel = function() {
+        model.setText(getText());
+    }
+
+    var setText = function(text) {
+        textAreaElement.value = text;
+    }
+
+    var getText = function() {
+        return textAreaElement.value;
+    }
+
+    var bindToControls = function() {
+        textAreaElement.onkeyup = updateModel;
+    }
+
+    init();
 }
 
 multiview.manualTextBoxView = function(model, dom, rootElement) {
+    var textAreaElement = rootElement.getElementsByTagName('textarea')[0];
+    var updateButton = rootElement.getElementsByTagName('input')[0];
 
+    var init = function() {
+        console.log("Initialising ManualTextBox view");
+        updateUI();
+        model.addListener(updateUI);
+        bindToControls();
+    }
+
+    var updateUI = function() {
+        if (getText() !== model.getText()) {
+            setText(model.getText());
+        }
+    }
+
+    var updateModel = function() {
+        model.setText(getText());
+    }
+
+    var setText = function(text) {
+        textAreaElement.value = text;
+    }
+
+    var getText = function() {
+        return textAreaElement.value;
+    }
+
+    var bindToControls = function() {
+        updateButton.onclick = updateModel;
+    }
+
+    init();
 }
 
 multiview.readOnlyDisplayView = function(model, dom, rootElement) {
 
     var textElement = rootElement.getElementsByTagName('p')[0];
 
-    var self = {
-
-    }
-
     var init = function() {
         console.log("Initialising ReadOnlyDisplay view");
         updateUI();
-        model.addListener(updateUI());
+        model.addListener(updateUI);
     }
 
     var updateUI = function() {
-        setText(model.getText());
+        if (getText() !== model.getText()) {
+            setText(model.getText());
+        }
     }
 
     var setText = function(text) {
         textElement.innerHTML = text;
     }
 
+    var getText = function() {
+        return textElement.innerHTML;
+    }
+
     init();
-    return self;
 }
 
 var controller = multiview.controller(document);
